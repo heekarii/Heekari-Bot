90f9588b71494f4e83cbf1a2b8d47232b64bf26f
heekarii <hantecude123@gmail.com>
2024-07-27 02:43:31 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'MTI2NjQ0ODIzMDI2MzI5NjA5NA.GuG9_v.0E13ArMApgsFNvFR1f083akrEZuO8-wABkDW-U';
+
+client.on('ready', () => {
+    console.log('{client.user.tag} has logged in.');
+    }   
+)
