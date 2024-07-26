03e783c1dfc80eb25182a90855834471a6efb670
heekarii <hantecude123@gmail.com>
2024-07-27 02:48:48 +0900

    Add: env 파일 추가

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'MTI2NjQ0ODIzMDI2MzI5NjA5NA.GuG9_v.0E13ArMApgsFNvFR1f083akrEZuO8-wABkDW-U';
+const token = process.env.TOKEN;
 
 client.on('ready', () => {
     console.log('{client.user.tag} has logged in.');
